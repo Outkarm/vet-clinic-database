70cf2c92e6d461d6a201e8caa2028b3c2ef825aa
Outkarm <jkpordje@gmail.com>
2023-03-11 16:52:22 +0000

    create and link multi tables to one

--- queries.sql
+++ queries.sql
@@ -69,3 +69,28 @@ SELECT Max (escape_attempts), neurtered from animals group by neurtered;
 SELECT Max (weight_kg), Min (weight_kg), species from animals group by species;
 
 SELECT AVG(escape_attempts), date_of_birth,name, species from animals WHERE (date_of_birth) between '1990-01-01' and '2000-12-31' group by species, date_of_birth, name;
+
+SELECT animals.name , owners.full_name FROM animals, owners where full_name='Melody Pond' and owners.id=animals.owner_id;
+
+
+
+SELECT animals.name animals , species.name species FROM animals, species WHERE species.id=animals.species_id AND species.name='Pokemon';
+
+
+
+SELECT animals.name animals , owners.full_name owners FROM animals right outer join owners on owners.id=animals.owner_id;
+
+
+SELECT count(animals.name) total_animals ,species.name species FROM animals inner join species on species.id=animals.species_id Group by species.name;
+
+
+SELECT animals.name animals , owners.full_name owners, species.name FROM animals inner join owners on owners.id=animals.owner_id inner join species on species.id=animals.species_id where  species.name='Digimon' and owners.full_name='Jennifer Orwell';
+
+
+SELECT owners.full_name, animals.name , animals.escape_attempts FROM animals inner join owners on owners.id=animals.owner_id where owners.full_name='Dean Winchester' and animals.escape_attempts=0;
+
+
+
+
+SELECT  count(animals.name) total_animals, owners.full_name FROM animals inner join owners on owners.id= animals.owner_id
+group by owners.full_name order by total_animals desc limit 1;
